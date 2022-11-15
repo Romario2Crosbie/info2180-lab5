46f7ab177e42d8818a336a65ff8f07b2dacf85f5
Romario2Crosbie <romario.anonymous2000@gmail.com>
2022-11-14 22:54:03 -0500

    everything works

--- world.js
+++ world.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function() {
     let lookupbtn = document.querySelector('#lookup');
     let ctry = document.querySelector('#country');
     let fName = 'world.php?country=';
+    let ctyButton = document.querySelector('#lookupcty');
+    let cName = '&lookup=cities';
 
 
     lookupbtn.addEventListener('click', function(element) {
@@ -21,4 +23,24 @@ window.addEventListener('load', function() {
             })
             .catch(error => console.log('There was an error: ' + error));
     });
-});
+
+    ctyButton.addEventListener('click', function(element){
+        element.preventDefault();
+
+        fetch(fName+ctry.value+cName)
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('something went wrong!')
+            }
+        })
+        .then(data => {
+            let btn = document.querySelector('#result');
+            btn.innerHTML = data;
+        })
+        .catch(error => console.log('There was an error: ' + error));
+
+    }) 
+       
+    });
--- world.php
+++ world.php
@@ -4,19 +4,72 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+error_reporting(0);
 $country = $_GET['country'];
+$lookup = $_GET['lookup'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php 
+if ($lookup == 'cities'){ 
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities inner join countries on cities.country_code = countries.code where countries.name like '%$country%' group by cities.name");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+  
+  ?>
+
+  <table class='table'>
+  <thread>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+  </thread>
+  <tbody>
+<?php foreach($results as $row): ?>
+      <tr>
+        <td><?=$row['name']; ?></td>
+        <td><?=$row['district']; ?></td>
+        <td><?=$row['population']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+    
+  <?php }
+  else {
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  ?>
+  <table class ='table'>
+  <thread>
+    <tr>
+      <th>Country Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  </thread>
+  <tbody>
+<?php foreach($results as $row): ?>
+      <tr>
+        <td><?=$row['name']; ?></td>
+        <td><?=$row['continent']; ?></td>
+        <td><?=$row['independence_year']; ?></td>
+        <td><?=$row['head_of_state']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+<?php } ?>
+
+
+
+
+
+      
+      
+
+
+
